2b822a4d6a7fd3a133b892f7049cfae7a13731a8
Samet İrkören <sametirkoren@gmail.com>
2021-01-08 17:01:40 +0300

    toast service for notifications

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,12 +13,21 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { ToastrModule } from 'ngx-toastr';
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    MemberListComponent,
+    MemberDetailComponent,
+    ListsComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
@@ -27,7 +36,10 @@ import { RegisterComponent } from './register/register.component';
     BrowserAnimationsModule,
     FormsModule,
     BsDropdownModule.forRoot(),
-    NgbModule
+    NgbModule,
+    ToastrModule.forRoot({
+      positionClass: 'toast-bottom-right'
+    })
 
   ],
   providers: [],
