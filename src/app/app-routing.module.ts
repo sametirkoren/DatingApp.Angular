5cda600c24f41245ebd0430fa84b5154f3f517b1
Samet İrkören <sametirkoren@gmail.com>
2021-01-14 16:52:27 +0300

    identity changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminPanelComponent } from './admin/admin-panel/admin-panel.component';
 import { HomeComponent } from './home/home.component';
 import { ListsComponent } from './lists/lists.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
+import { AdminGuard } from './_guards/admin.guard';
 import { AuthGuard } from './_guards/auth.guard';
 import { PreventUnsavedChangesGuard } from './_guards/prevent-unsaved-changes.guard';
 import { MemberDetailedResolver } from './_resolvers/member-detailed.resolver';
@@ -23,6 +25,7 @@ const routes: Routes = [
       {path: 'member/edit', component:MemberEditComponent , canDeactivate: [PreventUnsavedChangesGuard]},
       {path: 'lists', component:ListsComponent},
       {path: 'messages', component:MessagesComponent},
+      {path: 'admin', component:AdminPanelComponent , canActivate : [AdminGuard]},
     ]
   },
 
